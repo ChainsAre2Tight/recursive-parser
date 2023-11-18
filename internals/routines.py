c816c6eac8c6c7fa62d062645c49e858ad287d24
ChainsAre2Tight <124983704+ChainsAre2Tight@users.noreply.github.com>
2023-11-18 19:12:00 +0300

    renamed to routines.py

--- internals/routines.py
+++ internals/routines.py
@@ -2,12 +2,12 @@ import time
 
 from internals.parsing_utils.parser import Parser
 from internals.handlers import eventhandler
-from internals.objects import Page
+from internals.objects import *
 from internals.visualisation.graph_builder import GraphBuilder
 import pickle
 
 
-def perform_parsing(config):
+def perform_parsing_routine(config):
     eventhandler.new_status("Starting parsing routine...")
     start_time = time.time()
 
@@ -40,27 +40,31 @@ def perform_parsing(config):
             eventhandler.new_info(f"Finished in {round(time.time() - start_time, 3)} seconds")
 
             eventhandler.new_status(f"Dumping parsed data to {config.pickle_dump_file_name}...")
+            print(parsed_pages)
             with open(config.pickle_dump_file_name, 'wb') as f:
-                pickle.dump(parsed_pages, f, pickle.HIGHEST_PROTOCOL)
+                stringdata = str(parsed_pages)
+                pickle.dump(stringdata, f, pickle.HIGHEST_PROTOCOL)
                 eventhandler.new_status(f'Dump successful')
                 del parsed_pages
     except KeyboardInterrupt:
         exit_code = 'KeyboardInterrupt'
     finally:
         eventhandler.new_status(f'Parsing routine completed with exit code {exit_code}')
-        del eventhandler
 
 
-def construct_graph(config):
+def construct_graph_routine(config):
     eventhandler.new_status('Starting graph constructing routine...')
     exit_code = "Unhandled error"
 
     try:
         eventhandler.new_status(f"Trying to read data from {config.pickle_dump_file_name}...")
         with open(config.pickle_dump_file_name, 'rb') as f:
-            parsed_pages = pickle.load(f)
+            stringdata = pickle.load(f)
+            parsed_pages = eval(stringdata)
 
-        eventhandler.new_status(f"Successfully read dump, {len(parsed_pages.keys())} pages loaded")
+        num_of_pages_loaded = len(parsed_pages.keys())
+        eventhandler.new_status(f"Successfully read dump")
+        eventhandler.new_info(f"Read {num_of_pages_loaded} page{'s' if num_of_pages_loaded > 1 else ''}")
 
         eventhandler.new_status(f'Building graph...')
         graph, nodes = GraphBuilder.graph_from_parsed_pages(parsed_pages)
