beac0074f133b3bdfad6d147ba824f6ef1b1bab8
ChainsAre2Tight <124983704+ChainsAre2Tight@users.noreply.github.com>
2023-11-21 12:36:31 +0300

    added "both" option that performs both routines and task can now be passed as command line argument

--- main.py
+++ main.py
@@ -9,10 +9,17 @@ sys.setrecursionlimit(1000)
 def main():
     config = ConfigParser.parse()
 
-    task = input("? What task to perform? parse/export\n< ")
+    try:
+        task = sys.argv[1]
+    except IndexError:
+        task = input("? What task to perform? parse/export\n< ")
+
     if task == 'parse':
         perform_parsing_routine(config)
     elif task == 'export':
+        construct_graph_routine(config)
+    elif task == 'both':
+        perform_parsing_routine(config)
         construct_graph_routine(config)
     else:
         print('Unknown task. Leaving.')
